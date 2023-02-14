a098d46e08420e23de56be1e2a9e924247bc6f68
priyankac41 <pchandr1@chicagobooth.edu>
2023-02-14 03:58:28 +0000

    almost done with links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,13 @@ class UsersController < ApplicationController
   def index
     matching_users = User.all
     @list_of_users = matching_users.order({:username => :asc})
-    @name = params.fetch("path_username")
     render({:template => "user_templates/index.html.erb"})
+  end
+
+  def show
+    url_username = params.fetch("path_username")
+    matching_usernames = User.where({:username => url_username})
+    @the_user = matching_usernames.first
+    render({:template => "user_templates/show.html.erb"})
   end
 end
